7ab20bd601dd61ae4b7caf51ec2fae9bd60e74a8
Nishtha-06 <nmodi0402@gmail.com>
2026-08-03 23:19:59 +0530

    Refactor factorial count calculation in main

--- lab3_pr1.cpp
+++ lab3_pr1.cpp
@@ -1,26 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int fun(int n){
-    if(n<5){
-        return 0;
-    }
-    int i = 5;
-    int temp = n;
-    int count = 0;
-    while(temp>=5){
-
-        cout<<temp<<"/"<<i<<" = ";
-        temp = n/i;
-        i = i*5;
-        cout<<temp<<endl;
-        count = count + temp ;
-    }
-    return count;
-}
 int main(){
-    int n = 25;
-    // cin>>n;
+    int n,count=0;
+    cin>>n;
 
-    cout<<(fun(n));
+    while(n>=5){
+
+        n /= 5;
+        count += n;
+        
+    }
+    cout<<count;
 }
